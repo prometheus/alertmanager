17e4cf0d304fb7f7eb2b3f5b52573b8fffce314b
Sharif Nassar <snassar@bottlenose.com>
2015-09-02 14:41:57 -0700

    Fix version info string.
    Broken by the following commit
    e38bb7c Use Makefile.COMMON and add generated files.go

--- build_info.go
+++ build_info.go
@@ -19,17 +19,19 @@ import (
 
 // Build information. Populated by Makefile.
 var (
-	buildVersion string
-	buildBranch  string
-	buildUser    string
-	buildDate    string
-	goVersion    string
+	buildVersion  string
+	buildRevision string
+	buildBranch   string
+	buildUser     string
+	buildDate     string
+	goVersion     string
 )
 
 // BuildInfo encapsulates compile-time metadata about Prometheus made available
 // via go tool ld such that this can be reported on-demand.
 var BuildInfo = map[string]string{
 	"version":    buildVersion,
+	"revision":   buildRevision,
 	"branch":     buildBranch,
 	"user":       buildUser,
 	"date":       buildDate,
@@ -37,7 +39,7 @@ var BuildInfo = map[string]string{
 }
 
 var versionInfoTmpl = template.Must(template.New("version").Parse(
-	`alertmanager, version {{.version}} ({{.branch}})
+	`alertmanager, version {{.version}} ({{.branch}}@{{.revision}})
   build user:       {{.user}}
   build date:       {{.date}}
   go version:       {{.go_version}}
