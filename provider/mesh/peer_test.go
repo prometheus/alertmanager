d241016f956145340b6a165426eabf36c77b58c8
Fabian Reinartz <fab.reinartz@gmail.com>
2016-06-08 14:56:05 +0200

    proivder/mesh: add NotificationInfos Set test

--- provider/mesh/peer_test.go
+++ provider/mesh/peer_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/prometheus/alertmanager/types"
 	"github.com/prometheus/common/log"
 	"github.com/weaveworks/mesh"
 )
@@ -138,4 +139,115 @@ func TestNotificationInfosOnGossip(t *testing.T) {
 			t.Errorf("%v OnGossip %v: want %v, have %v", c.initial, c.msg, want, have)
 		}
 	}
+}
+
+func TestNotificationInfosSet(t *testing.T) {
+	var (
+		t0 = time.Now()
+		t1 = t0.Add(10 * time.Minute)
+		// t2 = t0.Add(20 * time.Minute)
+		// t3 = t0.Add(30 * time.Minute)
+	)
+	cases := []struct {
+		initial map[string]notificationEntry
+		input   []*types.NotifyInfo
+		update  map[string]notificationEntry
+		final   map[string]notificationEntry
+	}{
+		{
+			initial: map[string]notificationEntry{},
+			input: []*types.NotifyInfo{
+				{
+					Alert:     0x10,
+					Receiver:  "recv1",
+					Resolved:  false,
+					Timestamp: t0,
+				},
+			},
+			update: map[string]notificationEntry{
+				"0000000000000010:recv1": {false, t0},
+			},
+			final: map[string]notificationEntry{
+				"0000000000000010:recv1": {false, t0},
+			},
+		},
+		{
+			// In this testcase we the second input update is already state
+			// respective to the current state. We currently do not prune it
+			// from the update as it's not a common occurrence.
+			// The update is okay to propagate but the final state must correctly
+			// drop it.
+			initial: map[string]notificationEntry{
+				"0000000000000010:recv1": {false, t0},
+				"0000000000000010:recv2": {false, t1},
+			},
+			input: []*types.NotifyInfo{
+				{
+					Alert:     0x10,
+					Receiver:  "recv1",
+					Resolved:  true,
+					Timestamp: t1,
+				},
+				{
+					Alert:     0x10,
+					Receiver:  "recv2",
+					Resolved:  true,
+					Timestamp: t0,
+				},
+				{
+					Alert:     0x20,
+					Receiver:  "recv2",
+					Resolved:  false,
+					Timestamp: t0,
+				},
+			},
+			update: map[string]notificationEntry{
+				"0000000000000010:recv1": {true, t1},
+				"0000000000000010:recv2": {true, t0},
+				"0000000000000020:recv2": {false, t0},
+			},
+			final: map[string]notificationEntry{
+				"0000000000000010:recv1": {true, t1},
+				"0000000000000010:recv2": {false, t1},
+				"0000000000000020:recv2": {false, t0},
+			},
+		},
+	}
+
+	for _, c := range cases {
+		ni := NewNotificationInfos(log.Base())
+		tg := &testGossip{}
+		ni.Register(tg)
+		ni.st = &notificationState{set: c.initial}
+
+		if err := ni.Set(c.input...); err != nil {
+			t.Errorf("Insert failed: %s", err)
+			continue
+		}
+		// Verify the correct state afterwards.
+		if have := ni.st.set; !reflect.DeepEqual(have, c.final) {
+			t.Errorf("Wrong final state %v, expected %v", have, c.final)
+			continue
+		}
+
+		// Verify that we gossiped the correct update.
+		if have := tg.updates[0].(*notificationState).set; !reflect.DeepEqual(have, c.update) {
+			t.Errorf("Wrong gossip update %v, expected %v", have, c.update)
+			continue
+		}
+	}
+}
+
+// testGossip implements the mesh.Gossip interface. Received broadcast
+// updates are appended to a list.
+type testGossip struct {
+	updates []mesh.GossipData
+}
+
+func (g *testGossip) GossipUnicast(dst mesh.PeerName, msg []byte) error {
+	panic("not implemented")
+}
+
+func (g *testGossip) GossipBroadcast(update mesh.GossipData) {
+	g.updates = append(g.updates, update)
 }
