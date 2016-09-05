44bde96447e0edf5ed53a7320c7fd08d6deb101b
Fabian Reinartz <fab.reinartz@gmail.com>
2016-09-05 11:17:28 +0200

    examples: pretty print JSON in webhook

--- examples/webhook/echo.go
+++ examples/webhook/echo.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func main() {
 	http.ListenAndServe(":5001", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("received request:", r.Header)
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			panic(err)
 		}
 		defer r.Body.Close()
-		log.Println("request body:", string(b))
+		var buf bytes.Buffer
+		if err := json.Indent(&buf, b, " >", "  "); err != nil {
+			panic(err)
+		}
+		fmt.Println(buf.String())
 	}))
 }
