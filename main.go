1062b392eb901245e98701a76e7414dc76c19f9a
stuart nelson <stuartnelson3@gmail.com>
2017-01-17 03:41:13 +0100

    Server can run without rebuilding elm files

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 func main() {
 	var (
 		port = flag.String("port", "8080", "port to listen on")
+		dev  = flag.Bool("dev", true, "enable code rebuilding")
 	)
 	flag.Parse()
 
@@ -29,26 +30,28 @@ func main() {
 		http.ServeFile(w, r, "silences.json")
 	})
 
-	// Recompile the elm code whenever a change is detected.
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	if *dev {
+		// Recompile the elm code whenever a change is detected.
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
 
-	recompileFn := func() error {
-		cmd := exec.Command("elm-make", "src/Main.elm", "--output", "script.js")
-		// Look into the debugger
-		// cmd := exec.Command("elm-make", "src/Main.elm", "--output", "dist/script.js", "--debug")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		return cmd.Run()
+		recompileFn := func() error {
+			cmd := exec.Command("elm-make", "src/Main.elm", "--output", "script.js")
+			// Look into the debugger
+			// cmd := exec.Command("elm-make", "src/Main.elm", "--output", "dist/script.js", "--debug")
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			return cmd.Run()
+		}
+
+		go recompileFn()
+
+		watcher, err := guac.NewWatcher(ctx, "./src", recompileFn)
+		if err != nil {
+			log.Fatalf("error watching: %v", err)
+		}
+		go watcher.Run()
 	}
-
-	go recompileFn()
-
-	watcher, err := guac.NewWatcher(ctx, "./src", recompileFn)
-	if err != nil {
-		log.Fatalf("error watching: %v", err)
-	}
-	go watcher.Run()
 
 	log.Printf("starting listener on port %s", *port)
 	if err := http.ListenAndServe(":"+*port, nil); err != nil {
