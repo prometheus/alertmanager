83e496f040401c3eec20f6f000c52c18371dcdcd
stuart nelson <stuartnelson3@gmail.com>
2017-01-17 10:42:29 +0100

    Add DELETE to acceptable cors methods

--- api.go
+++ api.go
@@ -54,7 +54,7 @@ func init() {
 
 var corsHeaders = map[string]string{
 	"Access-Control-Allow-Headers":  "Accept, Authorization, Content-Type, Origin",
-	"Access-Control-Allow-Methods":  "GET, OPTIONS",
+	"Access-Control-Allow-Methods":  "GET, DELETE, OPTIONS",
 	"Access-Control-Allow-Origin":   "*",
 	"Access-Control-Expose-Headers": "Date",
 }
