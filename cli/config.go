0aa1b0992ded290afec92539b27d4790bf18eba1
Kellen Fox <kellen@cloudflare.com>
2017-03-06 15:22:02 -0800

    Small fixes to config system
    Remove commented out import
    Fix naming for VersionInfo

--- cli/config.go
+++ cli/config.go
@@ -10,7 +10,6 @@ import (
 	"github.com/prometheus/alertmanager/cli/format"
 	"github.com/prometheus/alertmanager/config"
 	"github.com/spf13/cobra"
-	//flag "github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -19,7 +18,7 @@ import (
 type Config struct {
 	Config      string            `json:"config"`
 	ConfigJSON  config.Config     `json:configJSON`
-	VersionINFO map[string]string `json:"versionInfo"`
+	VersionInfo map[string]string `json:"versionInfo"`
 	Uptime      time.Time         `json:"uptime"`
 }
 
