c5c46f85c04b9ff800cb913609509c109ae5fc11
Pierre Awaragi <xawarpi@ville.montreal.qc.ca>
2017-08-22 13:28:05 -0400

    use multipart createPart returned writer added error handling while creating parts removed unnecessary quotes from boundry string

--- notify/impl.go
+++ notify/impl.go
@@ -370,7 +370,7 @@ func (n *Email) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
 	multipartWriter := multipart.NewWriter(buffer)
 
 	fmt.Fprintf(wc, "Date: %s\r\n", time.Now().Format(time.RFC1123Z))
-	fmt.Fprintf(wc, "Content-Type: multipart/alternative;  boundary=\"%s\"\r\n", multipartWriter.Boundary())
+	fmt.Fprintf(wc, "Content-Type: multipart/alternative;  boundary=%s\r\n", multipartWriter.Boundary())
 
 
 	// TODO: Add some useful headers here, such as URL of the alertmanager
@@ -378,12 +378,15 @@ func (n *Email) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
 	fmt.Fprintf(wc, "\r\n")
 
 	// Html template
-	multipartWriter.CreatePart(textproto.MIMEHeader{"Content-Type": {"text/html; charset=UTF-8"}})
+	w, err := multipartWriter.CreatePart(textproto.MIMEHeader{"Content-Type": {"text/html; charset=UTF-8"}})
+	if err != nil {
+		return false, fmt.Errorf("creating part for html template: %s", err)
+	}
 	body, err := n.tmpl.ExecuteHTMLString(n.conf.HTML, data)
 	if err != nil {
 		return false, fmt.Errorf("executing email html template: %s", err)
 	}
-	_, err = buffer.WriteString(body)
+	_, err = w.Write([]byte(body))
 	if err != nil {
 		return true, err
 	}
@@ -391,12 +394,15 @@ func (n *Email) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
 	// Text template
 	// Last alternative based on recommendation in section 7.2.3 of w3 rfc1341 protocol
 	// https://www.w3.org/Protocols/rfc1341/7_2_Multipart.html
-	multipartWriter.CreatePart(textproto.MIMEHeader{"Content-Type": {"text/plain; charset=UTF-8"}})
+	w, err = multipartWriter.CreatePart(textproto.MIMEHeader{"Content-Type": {"text/plain; charset=UTF-8"}})
+	if err != nil {
+		return false, fmt.Errorf("create part for text template: %s", err)
+	}
 	body, err = n.tmpl.ExecuteTextString(n.conf.Text, data)
 	if err != nil {
 		return false, fmt.Errorf("executing email text template: %s", err)
 	}
-	_, err = buffer.WriteString(body)
+	_, err = w.Write([]byte(body))
 	if err != nil {
 		return true, err
 	}
