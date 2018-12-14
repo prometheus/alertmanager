534af887604e38ca8ec10a263f3343a861ded005
Sylvain Rabot <s.rabot@lectra.com>
2018-12-14 17:41:08 +0100

    template: add split function
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- template/template.go
+++ template/template.go
@@ -134,6 +134,9 @@ var DefaultFuncs = FuncMap{
 	"join": func(sep string, s []string) string {
 		return strings.Join(s, sep)
 	},
+	"split": func(sep string, s string) []string {
+		return strings.Split(s, sep)
+	},
 	"match": regexp.MatchString,
 	"safeHtml": func(text string) tmplhtml.HTML {
 		return tmplhtml.HTML(text)
