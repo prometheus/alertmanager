32b34f12891baef4956fc138e714dacbae64ab41
Hrishikesh Barman <hrishikeshbman@gmail.com>
2019-01-21 15:00:38 +0530

    added tools.go and updated genproto.sh
    Signed-off-by: Hrishikesh Barman <hrishikeshbman@gmail.com>

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -19,35 +19,24 @@ if ! [[ $(protoc --version) =~ "3.5.1" ]]; then
     exit 255
 fi
 
-# Exact version of plugins to build.
-PROTOC_GEN_GOGOFAST_SHA="971cbfd2e72b513a28c74af7462aee0800248d69"
-PROTOC_GEN_GRPC_ECOSYSTEM_SHA="e4b8a938efae14de11fd97311e873e989896348c"
-
 echo "installing plugins"
-pushd ${GOPATH}
-  go install "golang.org/x/tools/cmd/goimports"
-  go get -d -u "github.com/gogo/protobuf/protoc-gen-gogo"
-  go get -d -u "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-pushd ${GOPATH}/src/github.com/gogo/protobuf
-  git reset --hard "${PROTOC_GEN_GOGOFAST_SHA}"
-  go install "github.com/gogo/protobuf/protoc-gen-gogofast"
-pushd ${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway
-  git reset --hard "${PROTOC_GEN_GRPC_ECOSYSTEM_SHA}"
-  go install "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+GO111MODULE=on go mod download
 
-pushd -0 && dirs -c
+INSTALL_PKGS="golang.org/x/tools/cmd/goimports github.com/gogo/protobuf/protoc-gen-gogofast"
+for pkg in ${INSTALL_PKGS}; do
+    GO111MODULE=on go install -mod=vendor "$pkg"
+done
 
-GOGOPROTO_ROOT="${GOPATH}/src/github.com/gogo/protobuf"
+GOGOPROTO_ROOT="$(GO111MODULE=on go list -f '{{ .Dir }}' -m github.com/gogo/protobuf)"
 GOGOPROTO_PATH="${GOGOPROTO_ROOT}:${GOGOPROTO_ROOT}/protobuf"
-GRPC_GATEWAY_ROOT="${GOPATH}/src/github.com/grpc-ecosystem/grpc-gateway"
 
 DIRS="nflog/nflogpb silence/silencepb cluster/clusterpb"
 
+echo "generating files"
 for dir in ${DIRS}; do
     pushd ${dir}
         protoc --gogofast_out=plugins=grpc:. -I=. \
             -I="${GOGOPROTO_PATH}" \
-            -I="${GRPC_GATEWAY_ROOT}/third_party/googleapis" \
             *.proto
 
         sed -i.bak -E 's/import _ \"gogoproto\"//g' *.pb.go
