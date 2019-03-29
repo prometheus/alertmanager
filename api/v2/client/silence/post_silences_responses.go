89b0ff05743dbedfdfa8d2433c58ba4d5ddf7202
Paul Gier <pgier@redhat.com>
2019-03-29 06:29:00 -0500

    api/v2: Respond with 404 when silence ID is not found (#1816)
    When the client attempts to update a silence with a non-existent
    ID, respond with a 404 (Not Found) instead of a 400 (Bad Request).
    
    Signed-off-by: Paul Gier <pgier@redhat.com>

--- api/v2/client/silence/post_silences_responses.go
+++ api/v2/client/silence/post_silences_responses.go
@@ -52,6 +52,13 @@ func (o *PostSilencesReader) ReadResponse(response runtime.ClientResponse, consu
 		}
 		return nil, result
 
+	case 404:
+		result := NewPostSilencesNotFound()
+		if err := result.readResponse(response, consumer, o.formats); err != nil {
+			return nil, err
+		}
+		return nil, result
+
 	default:
 		return nil, runtime.NewAPIError("unknown error", response, response.Code())
 	}
@@ -104,6 +111,33 @@ func (o *PostSilencesBadRequest) Error() string {
 }
 
 func (o *PostSilencesBadRequest) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
+
+	// response payload
+	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
+		return err
+	}
+
+	return nil
+}
+
+// NewPostSilencesNotFound creates a PostSilencesNotFound with default headers values
+func NewPostSilencesNotFound() *PostSilencesNotFound {
+	return &PostSilencesNotFound{}
+}
+
+/*PostSilencesNotFound handles this case with default header values.
+
+A silence with the specified ID was not found
+*/
+type PostSilencesNotFound struct {
+	Payload string
+}
+
+func (o *PostSilencesNotFound) Error() string {
+	return fmt.Sprintf("[POST /silences][%d] postSilencesNotFound  %+v", 404, o.Payload)
+}
+
+func (o *PostSilencesNotFound) readResponse(response runtime.ClientResponse, consumer runtime.Consumer, formats strfmt.Registry) error {
 
 	// response payload
 	if err := consumer.Consume(response.Body(), &o.Payload); err != nil && err != io.EOF {
