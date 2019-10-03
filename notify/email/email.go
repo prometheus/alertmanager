56ffa4046f0cd3c894faa44d34d245733f907e13
PM <wugyresearcher@gmail.com>
2019-10-03 13:37:38 +0200

    check if message-id already exists
    Signed-off-by: PM <wugyresearcher@gmail.com>

--- notify/email/email.go
+++ notify/email/email.go
@@ -29,6 +29,7 @@ import (
 	"net/smtp"
 	"net/textproto"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
@@ -49,6 +50,8 @@ type Email struct {
 	tmpl   *template.Template
 	logger log.Logger
 }
+
+var reHeaderMessageId = regexp.MustCompile(`(?im)^message-id:`)
 
 // New returns a new Email notifier.
 func New(c *config.EmailConfig, t *template.Template, l log.Logger) *Email {
@@ -240,13 +243,15 @@ func (n *Email) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
 		fmt.Fprintf(buffer, "%s: %s\r\n", header, mime.QEncoding.Encode("utf-8", value))
 	}
 
+	if reHeaderMessageId.Find(buffer.Bytes()) == nil {
+		id, err := generateMessageID()
+		if err == nil {
+			fmt.Fprintf(buffer, "Message-Id: %s\r\n", id)
+		}
+	}
+
 	multipartBuffer := &bytes.Buffer{}
 	multipartWriter := multipart.NewWriter(multipartBuffer)
-
-	id, err := generateMessageID()
-	if err == nil {
-		fmt.Fprintf(buffer, "Message-Id: %s\r\n", id)
-	}
 
 	fmt.Fprintf(buffer, "Date: %s\r\n", time.Now().Format(time.RFC1123Z))
 	fmt.Fprintf(buffer, "Content-Type: multipart/alternative;  boundary=%s\r\n", multipartWriter.Boundary())
@@ -354,16 +359,14 @@ var maxBigInt = big.NewInt(math.MaxInt64)
 
 // generateMessageID generates and returns a string suitable for an RFC 2822
 // compliant Message-ID, e.g.:
-// <1444789264909237300.3464.1819418242800517193@DESKTOP01>
+// <1444789264909237300.1819418242800517193@DESKTOP01>
 //
 // The following parameters are used to generate a Message-ID:
 // - The nanoseconds since Epoch
-// - The calling PID
 // - A cryptographically random int64
 // - The sending hostname
 func generateMessageID() (string, error) {
 	t := time.Now().UnixNano()
-	pid := os.Getpid()
 	rint, err := rand.Int(rand.Reader, maxBigInt)
 	if err != nil {
 		return "", err
@@ -373,6 +376,5 @@ func generateMessageID() (string, error) {
 	if err != nil {
 		h = "localhost.localdomain"
 	}
-	msgid := fmt.Sprintf("<%d.%d.%d@%s>", t, pid, rint, h)
-	return msgid, nil
+	return fmt.Sprintf("<%d.%d@%s>", t, rint, h), nil
 }
