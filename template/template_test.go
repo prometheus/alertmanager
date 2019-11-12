9661fc32999208f09ce3ab67d9140875c2cf7481
Vincent Rischmann <vincent@rischmann.fr>
2019-11-12 16:02:44 +0100

    template: add the default template function stringSlice
    Since it's impossible to create a string slice in a Go template by
    default, add a function to work around this problem.
    
    The use case is to make it easy to call KV.Remove with an arbitrary
    slice inside a template.
    
    Signed-off-by: Vincent Rischmann <vincent@rischmann.fr>

--- template/template_test.go
+++ template/template_test.go
@@ -357,6 +357,19 @@ func TestTemplateExpansion(t *testing.T) {
 			in:    `{{ reReplaceAll "ab" "AB" "abcdabcda"}}`,
 			exp:   "ABcdABcda",
 		},
+		{
+			title: "Template using stringSlice",
+			in:    `{{ with .GroupLabels }}{{ with .Remove (stringSlice "key1" "key3") }}{{ .SortedPairs.Values }}{{ end }}{{ end }}`,
+			data: Data{
+				GroupLabels: KV{
+					"key1": "key1",
+					"key2": "key2",
+					"key3": "key3",
+					"key4": "key4",
+				},
+			},
+			exp: "[key2 key4]",
+		},
 	} {
 		tc := tc
 		t.Run(tc.title, func(t *testing.T) {
