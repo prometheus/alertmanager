fbdb0072dfc43bec5f751daa2ed4b06387246c74
yapo.yang <yang_yapo@126.com>
2019-12-02 22:22:48 +0800

    change email related configurations
    Signed-off-by: yapo.yang <yang_yapo@126.com>

--- notify/email/email.go
+++ notify/email/email.go
@@ -114,7 +114,7 @@ func (n *Email) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
 		success = false
 	)
 	if n.conf.Smarthost.Port == "465" {
-		tlsConfig, err := commoncfg.NewTLSConfig(&n.conf.TLSConfig)
+		tlsConfig, err := commoncfg.NewTLSConfig(n.conf.TLSConfig)
 		if err != nil {
 			return false, errors.Wrap(err, "parse TLS configuration")
 		}
@@ -161,7 +161,7 @@ func (n *Email) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
 			return true, errors.Errorf("'require_tls' is true (default) but %q does not advertise the STARTTLS extension", n.conf.Smarthost)
 		}
 
-		tlsConf, err := commoncfg.NewTLSConfig(&n.conf.TLSConfig)
+		tlsConf, err := commoncfg.NewTLSConfig(n.conf.TLSConfig)
 		if err != nil {
 			return false, errors.Wrap(err, "parse TLS configuration")
 		}
