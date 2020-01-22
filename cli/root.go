a2aa0cb5bf3e8f9a43721e7514547317a55fac25
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2020-01-22 14:53:19 +0100

    [#2160] Removed default assignment of env vars. (#2161)
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>

--- cli/root.go
+++ cli/root.go
@@ -89,7 +89,7 @@ func NewAlertmanagerClient(amURL *url.URL) *client.Alertmanager {
 // Execute is the main function for the amtool command
 func Execute() {
 	var (
-		app = kingpin.New("amtool", helpRoot).DefaultEnvars()
+		app = kingpin.New("amtool", helpRoot)
 	)
 
 	format.InitFormatFlags(app)
