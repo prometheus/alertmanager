277c9ed462f7c6da1b5f7173296e360e74e64496
Jason Cooper <deamwork@users.noreply.github.com>
2020-07-06 21:56:42 +0800

    notify: add markdown support for wechat (#2309)
    * notify: add markdown support for wechat
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * docs: update WeChat receiver configuration document
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * fix: check WeChat msgType, apply default if not present
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * chore: remove unnecessary comment
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * fix: simplify msgType process
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * docs: wechat configs document update
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * fix: apply error message suggestions
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * test: add test for regex
    
    Signed-off-by: Jason Cooper <master@deamwork.com>
    
    * fix: wechat message safe param
    
    Signed-off-by: Jason Cooper <master@deamwork.com>

--- config/notifiers.go
+++ config/notifiers.go
@@ -396,21 +396,38 @@ type WechatConfig struct {
 
 	HTTPConfig *commoncfg.HTTPClientConfig `yaml:"http_config,omitempty" json:"http_config,omitempty"`
 
-	APISecret Secret `yaml:"api_secret,omitempty" json:"api_secret,omitempty"`
-	CorpID    string `yaml:"corp_id,omitempty" json:"corp_id,omitempty"`
-	Message   string `yaml:"message,omitempty" json:"message,omitempty"`
-	APIURL    *URL   `yaml:"api_url,omitempty" json:"api_url,omitempty"`
-	ToUser    string `yaml:"to_user,omitempty" json:"to_user,omitempty"`
-	ToParty   string `yaml:"to_party,omitempty" json:"to_party,omitempty"`
-	ToTag     string `yaml:"to_tag,omitempty" json:"to_tag,omitempty"`
-	AgentID   string `yaml:"agent_id,omitempty" json:"agent_id,omitempty"`
+	APISecret   Secret `yaml:"api_secret,omitempty" json:"api_secret,omitempty"`
+	CorpID      string `yaml:"corp_id,omitempty" json:"corp_id,omitempty"`
+	Message     string `yaml:"message,omitempty" json:"message,omitempty"`
+	APIURL      *URL   `yaml:"api_url,omitempty" json:"api_url,omitempty"`
+	ToUser      string `yaml:"to_user,omitempty" json:"to_user,omitempty"`
+	ToParty     string `yaml:"to_party,omitempty" json:"to_party,omitempty"`
+	ToTag       string `yaml:"to_tag,omitempty" json:"to_tag,omitempty"`
+	AgentID     string `yaml:"agent_id,omitempty" json:"agent_id,omitempty"`
+	MessageType string `yaml:"message_type,omitempty" json:"message_type,omitempty"`
 }
+
+const wechatValidTypesRe = `^(text|markdown)$`
+
+var wechatTypeMatcher = regexp.MustCompile(wechatValidTypesRe)
 
 // UnmarshalYAML implements the yaml.Unmarshaler interface.
 func (c *WechatConfig) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	*c = DefaultWechatConfig
 	type plain WechatConfig
-	return unmarshal((*plain)(c))
+	if err := unmarshal((*plain)(c)); err != nil {
+		return err
+	}
+
+	if c.MessageType == "" {
+		c.MessageType = "text"
+	}
+
+	if !wechatTypeMatcher.MatchString(c.MessageType) {
+		return errors.Errorf("WeChat message type %q does not match valid options %s", c.MessageType, wechatValidTypesRe)
+	}
+
+	return nil
 }
 
 // OpsGenieConfig configures notifications via OpsGenie.
