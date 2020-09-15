5676534a63fd515fe660fe161c437ac10021a572
Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>
2020-09-15 12:06:06 +0200

    prometheus/alertmanager#2372 Move config reload metrics to Coordinator.Reload()
    Signed-off-by: Kevin Hellemun <17928966+OGKevin@users.noreply.github.com>

--- config/coordinator.go
+++ config/coordinator.go
@@ -16,12 +16,11 @@ package config
 import (
 	"crypto/md5"
 	"encoding/binary"
-	"sync"
-	"time"
-
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
+	"sync"
+	"time"
 )
 
 // Coordinator coordinates Alertmanager configurations beyond the lifetime of a
@@ -97,15 +96,10 @@ func (c *Coordinator) notifySubscribers() error {
 func (c *Coordinator) loadFromFile() error {
 	conf, err := LoadFile(c.configFilePath)
 	if err != nil {
-		c.configSuccessMetric.Set(0)
 		return err
 	}
 
 	c.config = conf
-	c.configSuccessMetric.Set(1)
-	c.configSuccessTimeMetric.Set(float64(time.Now().Unix()))
-	hash := md5HashAsMetricValue([]byte(c.config.original))
-	c.configHashMetric.Set(hash)
 
 	return nil
 }
@@ -126,6 +120,7 @@ func (c *Coordinator) Reload() error {
 			"file", c.configFilePath,
 			"err", err,
 		)
+		c.configSuccessMetric.Set(0)
 		return err
 	}
 	level.Info(c.logger).Log(
@@ -139,8 +134,14 @@ func (c *Coordinator) Reload() error {
 			"file", c.configFilePath,
 			"err", err,
 		)
+		c.configSuccessMetric.Set(0)
 		return err
 	}
+
+	c.configSuccessMetric.Set(1)
+	c.configSuccessTimeMetric.Set(float64(time.Now().Unix()))
+	hash := md5HashAsMetricValue([]byte(c.config.original))
+	c.configHashMetric.Set(hash)
 
 	return nil
 }
