9ab7cef7510e66c2f38775e76c758165133b9536
gotjosh <josue@grafana.com>
2021-03-29 19:22:06 +0100

    Fix tests
    Signed-off-by: gotjosh <josue@grafana.com>

--- api/v2/api_test.go
+++ api/v2/api_test.go
@@ -255,7 +255,7 @@ func TestAlertToOpenAPIAlert(t *testing.T) {
 			UpdatedAt: updated,
 		}
 	)
-	openAPIAlert := alertToOpenAPIAlert(alert, types.AlertStatus{State: types.AlertStateActive}, receivers)
+	openAPIAlert := AlertToOpenAPIAlert(alert, types.AlertStatus{State: types.AlertStateActive}, receivers)
 	require.Equal(t, &open_api_models.GettableAlert{
 		Annotations: open_api_models.LabelSet{},
 		Alert: open_api_models.Alert{
