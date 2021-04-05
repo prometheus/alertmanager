1d339ce27645a96539c0eadd933b82a9921ed23a
m-masataka <m.mizukoshi.wakuwaku@gmail.com>
2021-04-06 03:33:28 +0900

    feat: add silence logging option
    This code was originally written by m-masataka, but tweaked by
    anarcat.
    
    Tests werre written by WTheMeier
    
    Signed-off-by: Antoine Beaupré <anarcat@debian.org>

--- silence/silence.go
+++ silence/silence.go
@@ -26,6 +26,7 @@ import (
 	"reflect"
 	"regexp"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 
@@ -253,6 +254,7 @@ type Silences struct {
 	metrics   *metrics
 	retention time.Duration
 	limits    Limits
+	logging   bool
 
 	mtx       sync.RWMutex
 	st        state
@@ -416,6 +418,8 @@ type Options struct {
 	Retention time.Duration
 	Limits    Limits
 
+	Logging bool
+
 	// A logger used by background processing.
 	Logger  *slog.Logger
 	Metrics prometheus.Registerer
@@ -441,6 +445,7 @@ func New(o Options) (*Silences, error) {
 		logger:    promslog.NewNopLogger(),
 		retention: o.Retention,
 		limits:    o.Limits,
+		logging:   o.Logging,
 		broadcast: func([]byte) {},
 		st:        state{},
 	}
@@ -762,6 +767,9 @@ func (s *Silences) Set(sil *pb.Silence) error {
 		if err := s.checkSizeLimits(msil); err != nil {
 			return err
 		}
+		if s.logging {
+			s.logSilence("update silence", sil)
+		}
 		return s.setSilence(msil, now)
 	}
 
@@ -798,6 +806,9 @@ func (s *Silences) Set(sil *pb.Silence) error {
 		}
 	}
 
+	if s.logging {
+		s.logSilence("create silence", sil)
+	}
 	return s.setSilence(msil, now)
 }
 
@@ -857,6 +868,9 @@ func (s *Silences) expire(id string) error {
 		sil.EndsAt = now
 	}
 	sil.UpdatedAt = now
+	if s.logging {
+		s.logSilence("expire silence", sil)
+	}
 	return s.setSilence(s.toMeshSilence(sil), now)
 }
 
@@ -1271,4 +1285,36 @@ func openReplace(filename string) (*replaceFile, error) {
 		filename: filename,
 	}
 	return rf, nil
+}
+
+// logging silence status changes.
+//
+// XXX: This rewrites some code present in cli/format/format.go,
+// labelsMatcher() and cli/format/format_extended.go,
+// FormatSilences(). Refactoring this seems a little out of scope for
+// now.
+func (s *Silences) logSilence(msg string, sil *pb.Silence) {
+	var listMatchers []string
+	matcherTypeOperator := map[string]string{
+		"EQUAL":      "=",
+		"REGEXP":     "=~",
+		"NOT_EQUAL":  "!=",
+		"NOT_REGEXP": "!~",
+	}
+	for _, matcher := range sil.Matchers {
+		ms := []string{matcher.Name, matcherTypeOperator[matcher.Type.String()], matcher.Pattern}
+		m := strings.Join(ms, ``)
+		listMatchers = append(listMatchers, m)
+	}
+	strMatchers := strings.Join(listMatchers, `,`)
+
+	s.logger.Info(
+		msg,
+		"Id", sil.Id,
+		"CreatedBy", sil.CreatedBy,
+		"Comment", sil.Comment,
+		"StartsAt", sil.StartsAt,
+		"EndsAt", sil.EndsAt,
+		"Matchers", strMatchers,
+	)
 }
