f2428d490c5718337698a1e19db5f423f2b1ab1e
Tyler Reid <tyler.reid@grafana.com>
2021-06-22 14:43:41 -0500

    Use supplied user creds when creating an STS client
    Signed-off-by: Tyler Reid <tyler.reid@grafana.com>
    Signed-off-by: Kyle Brandt <kyle@grafana.com>

--- notify/sns/sns.go
+++ notify/sns/sns.go
@@ -93,6 +93,7 @@ func (n *Notifier) Notify(ctx context.Context, alert ...*types.Alert) (bool, err
 			stsSess, err = session.NewSessionWithOptions(session.Options{
 				Config: aws.Config{
 					Region: aws.String(n.conf.Sigv4.Region),
+					Credentials: creds,
 				},
 				Profile: n.conf.Sigv4.Profile,
 			})
