f66bbab4217da4df2fce1a2bed67ff32c173ece0
gotjosh <josue.abreu@gmail.com>
2022-06-17 13:13:05 +0100

    Fix tests after rebase
    Signed-off-by: gotjosh <josue.abreu@gmail.com>

--- provider/mem/mem_test.go
+++ provider/mem/mem_test.go
@@ -441,7 +441,7 @@ func TestAlerts_Count(t *testing.T) {
 
 	// When insert an alert, and then silence it. It shows up with the correct filter.
 	alerts.Put(a2)
-	marker.SetSilenced(a2.Fingerprint(), 1, []string{"1"}, nil)
+	marker.SetActiveOrSilenced(a2.Fingerprint(), 1, []string{"1"}, nil)
 	require.Equal(t, 1, countByState(types.AlertStateSuppressed))
 	require.Equal(t, 1, countTotal())
 
