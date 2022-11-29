026c048b5643074b23be841c320b8887be29a883
SuperQ <superq@gmail.com>
2022-11-29 17:16:21 +0100

    Fixup linting.
    Signed-off-by: SuperQ <superq@gmail.com>

--- pkg/labels/parse.go
+++ pkg/labels/parse.go
@@ -45,11 +45,12 @@ var (
 // this comma and whitespace will be trimmed.
 //
 // Examples for valid input strings:
-//   {foo = "bar", dings != "bums", }
-//   foo=bar,dings!=bums
-//   foo=bar, dings!=bums
-//   {quote="She said: \"Hi, ladies! That's gender-neutral…\""}
-//   statuscode=~"5.."
+//
+//	{foo = "bar", dings != "bums", }
+//	foo=bar,dings!=bums
+//	foo=bar, dings!=bums
+//	{quote="She said: \"Hi, ladies! That's gender-neutral…\""}
+//	statuscode=~"5.."
 //
 // See ParseMatcher for details on how an individual Matcher is parsed.
 func ParseMatchers(s string) ([]*Matcher, error) {
