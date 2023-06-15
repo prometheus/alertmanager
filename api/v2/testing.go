8288920eeb28eed8f8d9eafe01fea6f04b1afc01
Yijie Qin <qinyijie@amazon.com>
2023-06-14 23:49:34 -0400

    fix lint
    Signed-off-by: Yijie Qin <qinyijie@amazon.com>

--- api/v2/testing.go
+++ api/v2/testing.go
@@ -15,11 +15,12 @@ package v2
 
 import (
 	"encoding/json"
+	"testing"
+	"time"
+
 	"github.com/prometheus/alertmanager/provider"
 	"github.com/prometheus/alertmanager/types"
 	"github.com/prometheus/common/model"
-	"testing"
-	"time"
 
 	"github.com/go-openapi/strfmt"
 	"github.com/stretchr/testify/require"
