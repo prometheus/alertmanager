f93b50f7cada3718845d7f6b412c46b947397196
swoga <swoga@users.noreply.github.com>
2023-09-14 20:25:56 +0200

    feat: inhibit with label pairs
    Signed-off-by: swoga <swoga@users.noreply.github.com>
    Signed-off-by: MonkZ <MonkZ@users.noreply.github.com>

--- config/config.go
+++ config/config.go
@@ -966,9 +966,11 @@ type InhibitRule struct {
 	TargetMatchRE MatchRegexps `yaml:"target_match_re,omitempty" json:"target_match_re,omitempty"`
 	// TargetMatchers defines a set of label matchers that have to be fulfilled for target alerts.
 	TargetMatchers Matchers `yaml:"target_matchers,omitempty" json:"target_matchers,omitempty"`
-	// A set of labels that must be equal between the source and target alert
+	// A set of labels that must be equal between the source and target alert.
 	// for them to be a match.
 	Equal []string `yaml:"equal,omitempty" json:"equal,omitempty"`
+	// EqualPairs defines a set of pairs that have to be equal in the source and target alert.
+	EqualPairs []LabelPair `yaml:"equal_pairs,omitempty" json:"equal_pairs,omitempty"`
 }
 
 // UnmarshalYAML implements the yaml.Unmarshaler interface for InhibitRule.
@@ -998,6 +1000,12 @@ func (r *InhibitRule) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	}
 
 	return nil
+}
+
+// LabelPair defines a source / target label pair.
+type LabelPair struct {
+	SourceLabel model.LabelName `yaml:"source_label,omitempty" json:"source_label,omitempty"`
+	TargetLabel model.LabelName `yaml:"target_label,omitempty" json:"target_label,omitempty"`
 }
 
 // Receiver configuration provides configuration on how to contact a receiver.
