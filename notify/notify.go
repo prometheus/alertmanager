grafana/prometheus-alertmanager

Capture and expose notification delivery errors (#31)
This PR makes it possible to store the last error for each receiver in case of notification delivery failure. These errors are exposed via the `/api/v2/receivers` endpoint.

Co-authored-by: gotjosh <josue.abreu@gmail.com>

notify/notify.go
@@ -65,11 +65,16 @@ type Integration struct {
 	rs       ResolvedSender
 	name     string
 	idx      int
+
+	mtx                       sync.RWMutex
+	lastNotifyAttempt         time.Time
+	lastNotifyAttemptDuration model.Duration
+	lastNotifyAttemptError    error
 }
 
 // NewIntegration returns a new integration.
-func NewIntegration(notifier Notifier, rs ResolvedSender, name string, idx int) Integration {
-	return Integration{
+func NewIntegration(notifier Notifier, rs ResolvedSender, name string, idx int) *Integration {
+	return &Integration{
 		notifier: notifier,
 		rs:       rs,
 		name:     name,
@@ -95,6 +100,22 @@ func (i *Integration) Name() string {
 // Index returns the index of the integration.
 func (i *Integration) Index() int {
 	return i.idx
+}
+
+func (i *Integration) Report(start time.Time, duration model.Duration, notifyError error) {
+	i.mtx.Lock()
+	defer i.mtx.Unlock()
+
+	i.lastNotifyAttempt = start
+	i.lastNotifyAttemptDuration = duration
+	i.lastNotifyAttemptError = notifyError
+}
+
+func (i *Integration) GetReport() (time.Time, model.Duration, error) {
+	i.mtx.RLock()
+	defer i.mtx.RUnlock()
+
+	return i.lastNotifyAttempt, i.lastNotifyAttemptDuration, i.lastNotifyAttemptError
 }
 
 // String implements the Stringer interface.
@@ -318,7 +339,7 @@ func NewPipelineBuilder(r prometheus.Registerer) *PipelineBuilder {
 
 // New returns a map of receivers to Stages.
 func (pb *PipelineBuilder) New(
-	receivers map[string][]Integration,
+	receivers []*Receiver,
 	wait func() time.Duration,
 	inhibitor *inhibit.Inhibitor,
 	silencer *silence.Silencer,
@@ -334,32 +355,31 @@ func (pb *PipelineBuilder) New(
 	tms := NewTimeMuteStage(times)
 	ss := NewMuteStage(silencer)
 
-	for name := range receivers {
-		st := createReceiverStage(name, receivers[name], wait, notificationLog, pb.metrics)
-		rs[name] = MultiStage{ms, is, tas, tms, ss, st}
+	for _, r := range receivers {
+		st := createReceiverStage(r, wait, notificationLog, pb.metrics)
+		rs[r.groupName] = MultiStage{ms, is, tas, tms, ss, st}
 	}
 	return rs
 }
 
 // createReceiverStage creates a pipeline of stages for a receiver.
 func createReceiverStage(
-	name string,
-	integrations []Integration,
+	receiver *Receiver,
 	wait func() time.Duration,
 	notificationLog NotificationLog,
 	metrics *Metrics,
 ) Stage {
 	var fs FanoutStage
-	for i := range integrations {
+	for i := range receiver.integrations {
 		recv := &nflogpb.Receiver{
-			GroupName:   name,
-			Integration: integrations[i].Name(),
-			Idx:         uint32(integrations[i].Index()),
+			GroupName:   receiver.groupName,
+			Integration: receiver.integrations[i].Name(),
+			Idx:         uint32(receiver.integrations[i].Index()),
 		}
 		var s MultiStage
 		s = append(s, NewWaitStage(wait))
-		s = append(s, NewDedupStage(&integrations[i], notificationLog, recv))
-		s = append(s, NewRetryStage(integrations[i], name, metrics))
+		s = append(s, NewDedupStage(receiver.integrations[i], notificationLog, recv))
+		s = append(s, NewRetryStage(receiver.integrations[i], receiver.groupName, metrics))
 		s = append(s, NewSetNotifiesStage(notificationLog, recv))
 
 		fs = append(fs, s)
@@ -645,13 +665,13 @@ func (n *DedupStage) Exec(ctx context.Context, _ log.Logger, alerts ...*types.Al
 // RetryStage notifies via passed integration with exponential backoff until it
 // succeeds. It aborts if the context is canceled or timed out.
 type RetryStage struct {
-	integration Integration
+	integration *Integration
 	groupName   string
 	metrics     *Metrics
 }
 
 // NewRetryStage returns a new instance of a RetryStage.
-func NewRetryStage(i Integration, groupName string, metrics *Metrics) *RetryStage {
+func NewRetryStage(i *Integration, groupName string, metrics *Metrics) *RetryStage {
 	return &RetryStage{
 		integration: i,
 		groupName:   groupName,
@@ -720,8 +740,11 @@ func (r RetryStage) exec(ctx context.Context, l log.Logger, alerts ...*types.Ale
 		case <-tick.C:
 			now := time.Now()
 			retry, err := r.integration.Notify(ctx, sent...)
-			r.metrics.notificationLatencySeconds.WithLabelValues(r.integration.Name()).Observe(time.Since(now).Seconds())
+			duration := time.Since(now)
+
+			r.metrics.notificationLatencySeconds.WithLabelValues(r.integration.Name()).Observe(duration.Seconds())
 			r.metrics.numNotificationRequestsTotal.WithLabelValues(r.integration.Name()).Inc()
+			r.integration.Report(now, model.Duration(duration), err)
 			if err != nil {
 				r.metrics.numNotificationRequestsFailedTotal.WithLabelValues(r.integration.Name()).Inc()
 				if !retry {

prometheus/alertmanager

notify/notify.go
@@ -67,11 +67,16 @@ type Integration struct {
 	name         string
 	idx          int
 	receiverName string
+
+	mtx                       sync.RWMutex
+	lastNotifyAttempt         time.Time
+	lastNotifyAttemptDuration model.Duration
+	lastNotifyAttemptError    error
 }
 
 // NewIntegration returns a new integration.
-func NewIntegration(notifier Notifier, rs ResolvedSender, name string, idx int, receiverName string) Integration {
-	return Integration{
+func NewIntegration(notifier Notifier, rs ResolvedSender, name string, idx int, receiverName string) *Integration {
+	return &Integration{
 		notifier:     notifier,
 		rs:           rs,
 		name:         name,
@@ -98,6 +103,22 @@ func (i *Integration) Name() string {
 // Index returns the index of the integration.
 func (i *Integration) Index() int {
 	return i.idx
+}
+
+func (i *Integration) Report(start time.Time, duration model.Duration, notifyError error) {
+	i.mtx.Lock()
+	defer i.mtx.Unlock()
+
+	i.lastNotifyAttempt = start
+	i.lastNotifyAttemptDuration = duration
+	i.lastNotifyAttemptError = notifyError
+}
+
+func (i *Integration) GetReport() (time.Time, model.Duration, error) {
+	i.mtx.RLock()
+	defer i.mtx.RUnlock()
+
+	return i.lastNotifyAttempt, i.lastNotifyAttemptDuration, i.lastNotifyAttemptError
 }
 
 // String implements the Stringer interface.
@@ -302,7 +323,7 @@ func NewMetrics(r prometheus.Registerer, ff featurecontrol.Flagger) *Metrics {
 	return m
 }
 
-func (m *Metrics) InitializeFor(receiver map[string][]Integration) {
+func (m *Metrics) InitializeFor(receivers []*Receiver) {
 	if m.ff.EnableReceiverNamesInMetrics() {
 
 		// Reset the vectors to take into account receiver names changing after hot reloads.
@@ -312,8 +333,9 @@ func (m *Metrics) InitializeFor(receiver map[string][]Integration) {
 		m.notificationLatencySeconds.Reset()
 		m.numTotalFailedNotifications.Reset()
 
-		for name, integrations := range receiver {
-			for _, integration := range integrations {
+		for _, receiver := range receivers {
+			name := receiver.Name()
+			for _, integration := range receiver.Integrations() {
 
 				m.numNotifications.WithLabelValues(integration.Name(), name)
 				m.numNotificationRequestsTotal.WithLabelValues(integration.Name(), name)
@@ -370,7 +392,7 @@ func NewPipelineBuilder(r prometheus.Registerer, ff featurecontrol.Flagger) *Pip
 
 // New returns a map of receivers to Stages.
 func (pb *PipelineBuilder) New(
-	receivers map[string][]Integration,
+	receivers []*Receiver,
 	wait func() time.Duration,
 	inhibitor *inhibit.Inhibitor,
 	silencer *silence.Silencer,
@@ -386,9 +408,9 @@ func (pb *PipelineBuilder) New(
 	tms := NewTimeMuteStage(intervener)
 	ss := NewMuteStage(silencer)
 
-	for name := range receivers {
-		st := createReceiverStage(name, receivers[name], wait, notificationLog, pb.metrics)
-		rs[name] = MultiStage{ms, is, tas, tms, ss, st}
+	for _, r := range receivers {
+		st := createReceiverStage(r, wait, notificationLog, pb.metrics)
+		rs[r.groupName] = MultiStage{ms, is, tas, tms, ss, st}
 	}
 
 	pb.metrics.InitializeFor(receivers)
@@ -398,23 +420,22 @@ func (pb *PipelineBuilder) New(
 
 // createReceiverStage creates a pipeline of stages for a receiver.
 func createReceiverStage(
-	name string,
-	integrations []Integration,
+	receiver *Receiver,
 	wait func() time.Duration,
 	notificationLog NotificationLog,
 	metrics *Metrics,
 ) Stage {
 	var fs FanoutStage
-	for i := range integrations {
+	for i := range receiver.integrations {
 		recv := &nflogpb.Receiver{
-			GroupName:   name,
-			Integration: integrations[i].Name(),
-			Idx:         uint32(integrations[i].Index()),
+			GroupName:   receiver.groupName,
+			Integration: receiver.integrations[i].Name(),
+			Idx:         uint32(receiver.integrations[i].Index()),
 		}
 		var s MultiStage
 		s = append(s, NewWaitStage(wait))
-		s = append(s, NewDedupStage(&integrations[i], notificationLog, recv))
-		s = append(s, NewRetryStage(integrations[i], name, metrics))
+		s = append(s, NewDedupStage(receiver.integrations[i], notificationLog, recv))
+		s = append(s, NewRetryStage(receiver.integrations[i], receiver.groupName, metrics))
 		s = append(s, NewSetNotifiesStage(notificationLog, recv))
 
 		fs = append(fs, s)
@@ -708,14 +729,14 @@ func (n *DedupStage) Exec(ctx context.Context, _ log.Logger, alerts ...*types.Al
 // RetryStage notifies via passed integration with exponential backoff until it
 // succeeds. It aborts if the context is canceled or timed out.
 type RetryStage struct {
-	integration Integration
+	integration *Integration
 	groupName   string
 	metrics     *Metrics
 	labelValues []string
 }
 
 // NewRetryStage returns a new instance of a RetryStage.
-func NewRetryStage(i Integration, groupName string, metrics *Metrics) *RetryStage {
+func NewRetryStage(i *Integration, groupName string, metrics *Metrics) *RetryStage {
 	labelValues := []string{i.Name()}
 
 	if metrics.ff.EnableReceiverNamesInMetrics() {
@@ -803,6 +824,8 @@ func (r RetryStage) exec(ctx context.Context, l log.Logger, alerts ...*types.Ale
 			dur := time.Since(now)
 			r.metrics.notificationLatencySeconds.WithLabelValues(r.labelValues...).Observe(dur.Seconds())
 			r.metrics.numNotificationRequestsTotal.WithLabelValues(r.labelValues...).Inc()
+
+			r.integration.Report(now, model.Duration(dur), err)
 			if err != nil {
 				r.metrics.numNotificationRequestsFailedTotal.WithLabelValues(r.labelValues...).Inc()
 				if !retry {