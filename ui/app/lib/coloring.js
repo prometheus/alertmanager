151773c8af088744b2a6adde77a9a6370f4028e8
Vladimir Buyanov <b.vladimir@clickadu.com>
2024-02-26 17:27:22 +0200

    Fix Coloring

--- ui/app/lib/coloring.js
+++ ui/app/lib/coloring.js
@@ -11,36 +11,47 @@ const styles = {
     }
 }
 
-const elems = Array.from(document.getElementsByClassName("mb-3"));
-const alertsEl = elems.filter((el) => el.childNodes[0].classList.contains("mb-1"));
+while (true) {
+    const elems = Array.from(document.getElementsByClassName("mb-3"));
+    const alertsEl = elems.filter((el) => el.childNodes[0].classList.contains("mb-1"));
 
-let alerts = []
+    let alerts = []
 
-alertsEl.forEach((el) => {
-    let tags = []
-    let tagsKV = {}
-    Array.from(el.getElementsByClassName("btn-group")).forEach((e) => {
-        const key = e.childNodes[0].textContent.split("=")[0];
-        const value = e.childNodes[0].textContent.split("=")[1].replaceAll("\"", "")
-        let tag = {
-            el: e.childNodes[0],
-            key: key,
-            value: value,
-        }
-        tags.push(tag)
-        tagsKV[key] = value
-    });
-    alerts.push({el: el, tags: tags, tagsKV: tagsKV});
-});
-
-alerts.forEach((alert) => {
-    alert.tags.forEach((tag) => {
-        if (styles.hasOwnProperty(tag.key)) {
-            if (styles[tag.key].hasOwnProperty(tag.value)) {
-                Object.entries(styles[tag.key][tag.value]).forEach(([k, v]) => {
-                    tag.el.style.setProperty(k, v)
-                });
+    alertsEl.forEach((el) => {
+        let tags = []
+        let tagsKV = {}
+        Array.from(el.getElementsByClassName("btn-group")).forEach((e) => {
+            const key = e.childNodes[0].textContent.split("=")[0];
+            const value = e.childNodes[0].textContent.split("=")[1].replaceAll("\"", "")
+            let tag = {
+                el: e.childNodes[0],
+                key: key,
+                value: value,
             }
-        }
+            tags.push(tag)
+            tagsKV[key] = value
+        });
+        alerts.push({el: el, tags: tags, tagsKV: tagsKV});
     });
-});
+
+    alerts.forEach((alert) => {
+        alert.tags.forEach((tag) => {
+            if (styles.hasOwnProperty(tag.key)) {
+                if (styles[tag.key].hasOwnProperty(tag.value)) {
+                    Object.entries(styles[tag.key][tag.value]).forEach(([k, v]) => {
+                        tag.el.style.setProperty(k, v)
+                    });
+                }
+            }
+        });
+    });
+
+    // Удаляем имя ресивера с кнопки
+    alerts.forEach((alert) => {
+        alert.el.getElementsByClassName("mb-1")[0].childNodes[1].data="";
+    });
+
+    if (alerts.length > 0) {
+        break
+    }
+}
