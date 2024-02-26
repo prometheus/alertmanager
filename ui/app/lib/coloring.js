05ad7cab30521491d344f3e48e6d2ee11e20f407
Vladimir Buyanov <b.vladimir@clickadu.com>
2024-02-26 16:15:39 +0200

    Add coloring.js

---
+++ ui/app/lib/coloring.js
@@ -0,0 +1,46 @@
+
+const styles = {
+    "env": {
+        "prod": {"background-color": '#608cff', "font-weight": "bold", "color": "#000000"},
+        "stage": {"background-color": '#b3b3b3', "font-weight": "bold", "color": "#000000"},
+    },
+    "severity": {
+        "critical": {"background-color": "#ff5261", "font-weight": "bold", "color": "#000000"},
+        "warning": {"background-color": "#ffe16c", "font-weight": "bold", "color": "#000000"},
+        "info": {"background-color": "#bdff6c", "font-weight": "bold", "color": "#000000"},
+    }
+}
+
+const elems = Array.from(document.getElementsByClassName("mb-3"));
+const alertsEl = elems.filter((el) => el.childNodes[0].classList.contains("mb-1"));
+
+let alerts = []
+
+alertsEl.forEach((el) => {
+    let tags = []
+    let tagsKV = {}
+    Array.from(el.getElementsByClassName("btn-group")).forEach((e) => {
+        const key = e.childNodes[0].textContent.split("=")[0];
+        const value = e.childNodes[0].textContent.split("=")[1].replaceAll("\"", "")
+        let tag = {
+            el: e.childNodes[0],
+            key: key,
+            value: value,
+        }
+        tags.push(tag)
+        tagsKV[key] = value
+    });
+    alerts.push({el: el, tags: tags, tagsKV: tagsKV});
+});
+
+alerts.forEach((alert) => {
+    alert.tags.forEach((tag) => {
+        if (styles.hasOwnProperty(tag.key)) {
+            if (styles[tag.key].hasOwnProperty(tag.value)) {
+                Object.entries(styles[tag.key][tag.value]).forEach(([k, v]) => {
+                    tag.el.style.setProperty(k, v)
+                });
+            }
+        }
+    });
+});
