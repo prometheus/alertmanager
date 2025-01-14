dccef96fa2caf515ca3dda38b2ba590321a3a0c8
eth2353 <70237279+eth2353@users.noreply.github.com>
2025-01-14 15:14:49 +0100

    Update Settings for BS5
    Signed-off-by: eth2353 <70237279+eth2353@users.noreply.github.com>

--- ui/app/src/Views/Settings/Views.elm
+++ ui/app/src/Views/Settings/Views.elm
@@ -1,8 +1,8 @@
 module Views.Settings.Views exposing (view)
 
 import Html exposing (..)
-import Html.Attributes exposing (checked, class, for, id, type_, value)
-import Html.Events exposing (..)
+import Html.Attributes exposing (checked, class, for, id, name, type_, value)
+import Html.Events exposing (onInput)
 import Utils.DateTimePicker.Utils exposing (FirstDayOfWeek(..))
 import Views.Settings.Types exposing (Model, SettingsMsg(..))
 
@@ -10,32 +10,41 @@ import Views.Settings.Types exposing (Model, SettingsMsg(..))
 view : Model -> Html SettingsMsg
 view model =
     div []
-        [ div [ class "no-gutters" ]
-            [ label
-                [ for "fieldset" ]
-                [ text "First day of the week:" ]
+        [ -- Replaced "no-gutters" with either "g-0" or nothing at all,
+          -- depending on if you actually need row+column layout.
+          div [ class "g-0" ]
+            [ label [ for "fieldset" ] [ text "First day of the week:" ]
             , fieldset [ id "fieldset" ]
                 [ radio "Monday" (model.firstDayOfWeek == Monday) UpdateFirstDayOfWeek
                 , radio "Sunday" (model.firstDayOfWeek == Sunday) UpdateFirstDayOfWeek
                 ]
             , small [ class "form-text text-muted" ]
-                [ text "Note: This setting is saved in local storage of your browser"
-                ]
+                [ text "Note: This setting is saved in local storage of your browser" ]
             ]
         ]
 
 
 radio : String -> Bool -> (String -> msg) -> Html msg
-radio radioValue isChecked msg =
-    label [ class "mt-1 ml-1 custom-control custom-radio" ]
+radio radioValue isChecked toMsg =
+    let
+        -- Generate an ID based on the radioValue
+        radioId =
+            "radio-" ++ radioValue
+    in
+    div [ class "form-check form-check-inline ms-1 mt-1" ]
         [ input
-            [ type_ "checkbox"
-            , class "custom-control-input"
+            [ type_ "radio"
+            , class "form-check-input"
+            , id radioId
+            , name "firstDayOfWeek"
             , checked isChecked
             , value radioValue
-            , onInput msg
+            , onInput toMsg
             ]
             []
-        , span [ class "custom-control-indicator" ] []
-        , span [ class "custom-control-description" ] [ text radioValue ]
+        , label
+            [ class "form-check-label"
+            , Html.Attributes.for radioId
+            ]
+            [ text radioValue ]
         ]
