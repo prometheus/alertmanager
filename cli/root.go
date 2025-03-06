6d721e26a97399d0069bcc57804999a38d4e8394
Jan-Otto Kröpke <mail@jkroepke.de>
2025-03-06 23:59:34 +0100

    revert tests
    Signed-off-by: Jan-Otto Kröpke <mail@jkroepke.de>

--- cli/root.go
+++ cli/root.go
@@ -52,6 +52,7 @@ var (
 func initMatchersCompat(_ *kingpin.ParseContext) error {
 	promslogConfig := &promslog.Config{Writer: os.Stdout}
 	if verbose {
+		promslogConfig.Level = &promslog.AllowedLevel{}
 		_ = promslogConfig.Level.Set("debug")
 	}
 	logger := promslog.New(promslogConfig)
