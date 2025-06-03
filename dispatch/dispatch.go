dc4d855f22b886fd8a7d79bf083f19fc4b4b5864
Tito Lins <tito.linsesilva@grafana.com>
2025-06-03 18:00:12 +0200

    add group fingerprint to dispatch logs (#114)

--- dispatch/dispatch.go
+++ dispatch/dispatch.go
@@ -455,7 +455,7 @@ func newAggrGroup(ctx context.Context, labels model.LabelSet, r *Route, to func(
 	}
 	ag.ctx, ag.cancel = context.WithCancel(ctx)
 
-	ag.logger = log.With(logger, "aggrGroup", ag)
+	ag.logger = log.With(logger, "aggrGroup", ag, "group_fingerprint", ag.fingerprint())
 
 	// Set an initial one-time wait before flushing
 	// the first batch of notifications.
