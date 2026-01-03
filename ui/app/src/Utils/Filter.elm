df2de1380d35a5952658a8a45247d4579f754019
kkh <kkhdevs@gmail.com>
2026-01-04 04:34:30 +0900

    ui: escape matcher values in filters
    Signed-off-by: kkh <kkhdevs@gmail.com>

--- ui/app/src/Utils/Filter.elm
+++ ui/app/src/Utils/Filter.elm
@@ -279,7 +279,7 @@ stringifyMatcher { key, op, value } =
                 |> Maybe.withDefault ""
            )
         ++ "\""
-        ++ value
+        ++ escapeMatcherValue value
         ++ "\""
 
 
@@ -341,7 +341,7 @@ string separator =
         |. Parser.loop separator stringHelp
         |> Parser.getChompedString
         -- Remove quotes
-        |> Parser.map (String.dropLeft 1 >> String.dropRight 1)
+        |> Parser.map (String.dropLeft 1 >> String.dropRight 1 >> unescapeMatcherValue)
 
 
 stringHelp : Char -> Parser (Parser.Step Char ())
@@ -355,6 +355,66 @@ stringHelp separator =
         , Parser.succeed (Parser.Loop separator)
             |. Parser.chompIf (\char -> char /= '\\' && char /= separator)
         ]
+
+
+escapeMatcherValue : String -> String
+escapeMatcherValue value =
+    String.foldl
+        (\char acc ->
+            case char of
+                '\\' ->
+                    "\\\\" :: acc
+
+                '"' ->
+                    "\\\"" :: acc
+
+                '\n' ->
+                    "\\n" :: acc
+
+                _ ->
+                    String.fromChar char :: acc
+        )
+        []
+        value
+        |> List.reverse
+        |> String.join ""
+
+
+unescapeMatcherValue : String -> String
+unescapeMatcherValue value =
+    let
+        step char ( escaped, acc ) =
+            if escaped then
+                case char of
+                    'n' ->
+                        ( False, '\n' :: acc )
+
+                    '"' ->
+                        ( False, '"' :: acc )
+
+                    '\\' ->
+                        ( False, '\\' :: acc )
+
+                    _ ->
+                        ( False, char :: '\\' :: acc )
+
+            else if char == '\\' then
+                ( True, acc )
+
+            else
+                ( False, char :: acc )
+
+        ( escaped, acc ) =
+            String.foldl step ( False, [] ) value
+    in
+    (if escaped then
+        '\\' :: acc
+
+     else
+        acc
+    )
+        |> List.reverse
+        |> String.fromList
 
 
 isVarChar : Char -> Bool
