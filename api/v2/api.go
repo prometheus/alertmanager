2f032b0df77f3034dedca5bcafdc921dedc8da29
mehrdadbn9 <mehrdadbiukian@gmail.com>
2026-02-09 11:59:43 +0330

    Fix incorrect error response types in API handlers
    - Fixed getAlertGroupsHandler internal server error to include error body with error message
    - Fixed getSilencesHandler to use correct response type (GetSilencesBadRequest instead of GetAlertGroupsBadRequest)
    - Addresses reviewer feedback on PR #4984
    
    Signed-off-by: mehrdadbn9 <mehrdadbiukian@gmail.com>

--- api/v2/api.go
+++ api/v2/api.go
@@ -264,8 +264,8 @@ func (api *API) getAlertsHandler(params alert_ops.GetAlertsParams) middleware.Re
 	if err != nil {
 		logger.Debug("Failed to parse matchers", "err", err)
 		errorMsg := err.Error()
-		body := &silence_ops.GetSilencesBadRequestBody{Error: &errorMsg}
-		return silence_ops.NewGetSilencesBadRequest().WithPayload(body)
+		body := &alert_ops.GetAlertsBadRequestBody{Error: &errorMsg}
+		return alert_ops.NewGetAlertsBadRequest().WithPayload(body)
 	}
 
 	if params.Receiver != nil {
@@ -558,8 +558,8 @@ func (api *API) getSilencesHandler(params silence_ops.GetSilencesParams) middlew
 	if err != nil {
 		logger.Debug("Failed to parse matchers", "err", err)
 		errorMsg := err.Error()
-		body := &alertgroup_ops.GetAlertGroupsBadRequestBody{Error: &errorMsg}
-		return alertgroup_ops.NewGetAlertGroupsBadRequest().WithPayload(body)
+		body := &silence_ops.GetSilencesBadRequestBody{Error: &errorMsg}
+		return silence_ops.NewGetSilencesBadRequest().WithPayload(body)
 	}
 
 	psils, _, err := api.silences.Query(ctx)
