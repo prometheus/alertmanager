668647096e33ae31345071da6ced58563efea028
Joe Adams <github@joeadams.io>
2026-02-10 22:40:58 -0500

    Lint fix
    Signed-off-by: Joe Adams <github@joeadams.io>

--- ui/mantine-ui/src/components/ThemeSelector.tsx
+++ ui/mantine-ui/src/components/ThemeSelector.tsx
@@ -1,37 +1,27 @@
-import { useMantineColorScheme, rem, ActionIcon } from "@mantine/core";
-import {
-  IconMoonFilled,
-  IconSunFilled,
-  IconBrightnessFilled,
-} from "@tabler/icons-react";
-import { FC } from "react";
+import { FC } from 'react';
+import { IconBrightnessFilled, IconMoonFilled, IconSunFilled } from '@tabler/icons-react';
+import { ActionIcon, rem, useMantineColorScheme } from '@mantine/core';
 
 export const ThemeSelector: FC = () => {
   const { colorScheme, setColorScheme } = useMantineColorScheme();
   const iconProps = {
-    style: { width: rem(20), height: rem(20), display: "block" },
+    style: { width: rem(20), height: rem(20), display: 'block' },
     stroke: 1.5,
   };
 
   return (
     <ActionIcon
       color="gray"
-      title={`Switch to ${colorScheme === "light" ? "dark" : colorScheme === "dark" ? "browser-preferred" : "light"} theme`}
-      aria-label={`Switch to ${colorScheme === "light" ? "dark" : colorScheme === "dark" ? "browser-preferred" : "light"} theme`}
+      title={`Switch to ${colorScheme === 'light' ? 'dark' : colorScheme === 'dark' ? 'browser-preferred' : 'light'} theme`}
+      aria-label={`Switch to ${colorScheme === 'light' ? 'dark' : colorScheme === 'dark' ? 'browser-preferred' : 'light'} theme`}
       size={32}
       onClick={() =>
-        setColorScheme(
-          colorScheme === "light"
-            ? "dark"
-            : colorScheme === "dark"
-              ? "auto"
-              : "light"
-        )
+        setColorScheme(colorScheme === 'light' ? 'dark' : colorScheme === 'dark' ? 'auto' : 'light')
       }
     >
-      {colorScheme === "light" ? (
+      {colorScheme === 'light' ? (
         <IconSunFilled {...iconProps} />
-      ) : colorScheme === "dark" ? (
+      ) : colorScheme === 'dark' ? (
         <IconMoonFilled {...iconProps} />
       ) : (
         <IconBrightnessFilled {...iconProps} />
