de4478ac015cf34ce2d37001269877b07900fc75
Solomon Jacobs <solomonjacobs@protonmail.com>
2026-02-11 23:05:29 +0100

    chore: tiny shell improvements
    * stricter error handling
    * remove unnecessary install
    * suppress noisy output from `pushd` and `popd`
    * don't use regex on fixed string
    
    Co-authored-by: Guido Trotter <guido@hudson-trading.com>
    Signed-off-by: Solomon Jacobs <solomonjacobs@protonmail.com>

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
-#
 # Generate all protobuf bindings.
-# Run from repository root.
-set -e
-set -u
+set -euo pipefail
+shopt -s failglob
 
-if ! [[ "$0" =~ "scripts/genproto.sh" ]]; then
+if ! [[ "$0" = "scripts/genproto.sh" ]]; then
   echo "must be run from repository root"
   exit 255
 fi
 
-pushd "internal/tools"
-INSTALL_PKGS="github.com/bufbuild/buf/cmd/buf golang.org/x/tools/cmd/goimports google.golang.org/protobuf/cmd/protoc-gen-go"
+pushd "internal/tools" >/dev/null
+INSTALL_PKGS="github.com/bufbuild/buf/cmd/buf google.golang.org/protobuf/cmd/protoc-gen-go"
 for pkg in ${INSTALL_PKGS}; do
   go install "$pkg"
 done
-popd
+popd >/dev/null
 
 echo "generating files"
 buf dep update
