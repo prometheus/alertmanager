65131d5b5cf3899965daf6bd77f35df101b48d5f
Solomon Jacobs <solomonjacobs@protonmail.com>
2026-02-21 20:34:10 +0100

    chore (internal): remove `goimports`
    The usage of this tool was removed in this commit:
    a3a156d6f2f218fe2985925f63ddf5e8224eb20a
    
    Signed-off-by: Solomon Jacobs <solomonjacobs@protonmail.com>

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -20,6 +20,5 @@ package tools
 import (
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/go-swagger/go-swagger/cmd/swagger"
-	_ "golang.org/x/tools/cmd/goimports"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
