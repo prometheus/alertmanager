ba2419f4ce212b515d4bec0ebf21167b811933f4
Joe Adams <github@joeadams.io>
2026-02-21 15:19:49 -0500

    fix lint number precision
    Signed-off-by: Joe Adams <github@joeadams.io>

--- ui/mantine-ui/test-utils/render.tsx
+++ ui/mantine-ui/test-utils/render.tsx
@@ -1,13 +1,24 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { render as testingLibraryRender } from '@testing-library/react';
 import { MantineProvider } from '@mantine/core';
 import { theme } from '../src/theme';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
+
 export function render(ui: React.ReactNode) {
   return testingLibraryRender(ui, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
-      <MantineProvider theme={theme} env="test">
-        {children}
-      </MantineProvider>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider theme={theme} env="test">
+          {children}
+        </MantineProvider>
+      </QueryClientProvider>
     ),
   });
 }
