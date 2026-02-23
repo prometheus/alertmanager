7ee0ef7031cbc6d5a6d01dcfac3dfbf90c102249
Siavash Safi <siavash@cloudflare.com>
2026-02-23 16:42:59 +0100

    chore: add alert package, deprecate alert in types
    The `types` package is a well-known Go anti-pattern.
    This is a change from a series that split the `types` package.
    
    This change adds a new `alert` package which only includes alert types.
    The same types are re-exported from `types` for backward compatibility,
    and marked as deprecated.
    
    Signed-off-by: Siavash Safi <siavash@cloudflare.com>

---
+++ alert/validate.go
@@ -0,0 +1,34 @@
+// Copyright The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package alert
+
+import (
+	"fmt"
+
+	"github.com/prometheus/common/model"
+
+	"github.com/prometheus/alertmanager/matcher/compat"
+)
+
+func validateLs(ls model.LabelSet) error {
+	for ln, lv := range ls {
+		if !compat.IsValidLabelName(ln) {
+			return fmt.Errorf("invalid name %q", ln)
+		}
+		if !lv.IsValid() {
+			return fmt.Errorf("invalid value %q", lv)
+		}
+	}
+	return nil
+}
--- types/types.go
+++ types/types.go
@@ -15,7 +15,6 @@ package types
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"sync"
 	"time"
@@ -23,31 +22,33 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/model"
 
-	"github.com/prometheus/alertmanager/matcher/compat"
+	"github.com/prometheus/alertmanager/alert"
 	"github.com/prometheus/alertmanager/pkg/labels"
 )
 
-// AlertState is used as part of AlertStatus.
-type AlertState string
+// Deprecated: Use alert.Alert directly.
+type Alert = alert.Alert
 
-// Possible values for AlertState.
-const (
-	AlertStateUnprocessed AlertState = "unprocessed"
-	AlertStateActive      AlertState = "active"
-	AlertStateSuppressed  AlertState = "suppressed"
-)
+// Deprecated: Use alert.AlertSlice directly.
+type AlertSlice = alert.AlertSlice
 
-// AlertStatus stores the state of an alert and, as applicable, the IDs of
-// silences silencing the alert and of other alerts inhibiting the alert. Note
-// that currently, SilencedBy is supposed to be the complete set of the relevant
-// silences while InhibitedBy may contain only a subset of the inhibiting alerts
-// – in practice exactly one ID. (This somewhat confusing semantics might change
-// in the future.)
-type AlertStatus struct {
-	State       AlertState `json:"state"`
-	SilencedBy  []string   `json:"silencedBy"`
-	InhibitedBy []string   `json:"inhibitedBy"`
-}
+// Deprecated: Use alert.Alerts directly.
+var Alerts = alert.Alerts
+
+// Deprecated: Use alert.AlertState constants directly.
+type AlertState = alert.AlertState
+
+// Deprecated: Use alert.AlertStateActive directly.
+const AlertStateActive AlertState = alert.AlertStateActive
+
+// Deprecated: Use alert.AlertStateSuppressed directly.
+const AlertStateSuppressed AlertState = alert.AlertStateSuppressed
+
+// Deprecated: Use alert.AlertStateUnprocessed directly.
+const AlertStateUnprocessed AlertState = alert.AlertStateUnprocessed
+
+// Deprecated: Use alert.AlertStatus directly.
+type AlertStatus = alert.AlertStatus
 
 // groupStatus stores the state of the group, and, as applicable, the names
 // of all active and mute time intervals that are muting it.
@@ -342,123 +343,6 @@ func (e *MultiError) Error() string {
 		es = append(es, err.Error())
 	}
 	return strings.Join(es, "; ")
-}
-
-// Alert wraps a model.Alert with additional information relevant
-// to internal of the Alertmanager.
-// The type is never exposed to external communication and the
-// embedded alert has to be sanitized beforehand.
-type Alert struct {
-	model.Alert
-
-	// The authoritative timestamp.
-	UpdatedAt time.Time
-	Timeout   bool
-}
-
-func validateLs(ls model.LabelSet) error {
-	for ln, lv := range ls {
-		if !compat.IsValidLabelName(ln) {
-			return fmt.Errorf("invalid name %q", ln)
-		}
-		if !lv.IsValid() {
-			return fmt.Errorf("invalid value %q", lv)
-		}
-	}
-	return nil
-}
-
-// Validate overrides the same method in model.Alert to allow UTF-8 labels.
-// This can be removed once prometheus/common has support for UTF-8.
-func (a *Alert) Validate() error {
-	if a.StartsAt.IsZero() {
-		return fmt.Errorf("start time missing")
-	}
-	if !a.EndsAt.IsZero() && a.EndsAt.Before(a.StartsAt) {
-		return fmt.Errorf("start time must be before end time")
-	}
-	if len(a.Labels) == 0 {
-		return fmt.Errorf("at least one label pair required")
-	}
-	if err := validateLs(a.Labels); err != nil {
-		return fmt.Errorf("invalid label set: %w", err)
-	}
-	if err := validateLs(a.Annotations); err != nil {
-		return fmt.Errorf("invalid annotations: %w", err)
-	}
-	return nil
-}
-
-// AlertSlice is a sortable slice of Alerts.
-type AlertSlice []*Alert
-
-func (as AlertSlice) Less(i, j int) bool {
-	// Look at labels.job, then labels.instance.
-	for _, overrideKey := range [...]model.LabelName{"job", "instance"} {
-		iVal, iOk := as[i].Labels[overrideKey]
-		jVal, jOk := as[j].Labels[overrideKey]
-		if !iOk && !jOk {
-			continue
-		}
-		if !iOk {
-			return false
-		}
-		if !jOk {
-			return true
-		}
-		if iVal != jVal {
-			return iVal < jVal
-		}
-	}
-	return as[i].Labels.Before(as[j].Labels)
-}
-func (as AlertSlice) Swap(i, j int) { as[i], as[j] = as[j], as[i] }
-func (as AlertSlice) Len() int      { return len(as) }
-
-// Alerts turns a sequence of internal alerts into a list of
-// exposable model.Alert structures.
-func Alerts(alerts ...*Alert) model.Alerts {
-	res := make(model.Alerts, 0, len(alerts))
-	for _, a := range alerts {
-		v := a.Alert
-		// If the end timestamp is not reached yet, do not expose it.
-		if !a.Resolved() {
-			v.EndsAt = time.Time{}
-		}
-		res = append(res, &v)
-	}
-	return res
-}
-
-// Merge merges the timespan of two alerts based and overwrites annotations
-// based on the authoritative timestamp.  A new alert is returned, the labels
-// are assumed to be equal.
-func (a *Alert) Merge(o *Alert) *Alert {
-	// Let o always be the younger alert.
-	if o.UpdatedAt.Before(a.UpdatedAt) {
-		return o.Merge(a)
-	}
-
-	res := *o
-
-	// Always pick the earliest starting time.
-	if a.StartsAt.Before(o.StartsAt) {
-		res.StartsAt = a.StartsAt
-	}
-
-	if o.Resolved() {
-		// The latest explicit resolved timestamp wins if both alerts are effectively resolved.
-		if a.Resolved() && a.EndsAt.After(o.EndsAt) {
-			res.EndsAt = a.EndsAt
-		}
-	} else {
-		// A non-timeout timestamp always rules if it is the latest.
-		if a.EndsAt.After(o.EndsAt) && !a.Timeout {
-			res.EndsAt = a.EndsAt
-		}
-	}
-
-	return &res
 }
 
 // A Muter determines whether a given label set is muted. Implementers that
