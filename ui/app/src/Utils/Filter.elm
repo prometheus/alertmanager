0c9d5470d3e2bb88c68c937d7f028f79ecf26426
Victor Bogo <victor@37signals.com>
2026-03-20 11:02:23 -0300

    introduce isSpace that validates Latin-1, Unicode and ASCII whitespace characters and use that alongside the syntaxChars list to detect any reserved character
    Signed-off-by: Victor Bogo <victor@37signals.com>

--- ui/app/src/Utils/Filter.elm
+++ ui/app/src/Utils/Filter.elm
@@ -318,8 +318,8 @@ item : Parser Matcher
 item =
     Parser.succeed Matcher
         |= Parser.variable
-            { start = \c -> not (Set.member c reservedChars)
-            , inner = \c -> not (Set.member c reservedChars)
+            { start = \c -> not (isReservedChar c)
+            , inner = \c -> not (isReservedChar c)
             , reserved = Set.empty
             }
         |= (matchers
@@ -363,30 +363,39 @@ stringHelp separator =
         ]
 
 
-reservedChars : Set.Set Char
-reservedChars =
-    -- Matches Go's unicode.IsSpace (for Latin-1) plus matcher syntax characters
-    -- See: https://pkg.go.dev/unicode#IsSpace
-    Set.fromList
-        [ '{'
-        , '}'
-        , '!'
-        , '='
-        , '~'
-        , ','
-        , '\\'
-        , '"'
-        , '\''
-        , '`'
-        , ' '
-        , '\t'
-        , '\n'
-        , '\u{000B}'
-        , '\u{000C}'
-        , '\u{000D}'
-        , '\u{0085}'
-        , '\u{00A0}'
-        ]
+isReservedChar : Char -> Bool
+isReservedChar c =
+    isSpace c || Set.member c syntaxChars
+
+
+{-| Matches Go's unicode.IsSpace.
+See: <https://pkg.go.dev/unicode#IsSpace>
+-}
+isSpace : Char -> Bool
+isSpace c =
+    let
+        code =
+            Char.toCode c
+    in
+    -- ASCII whitespace: tab, newline, vertical tab, form feed, carriage return
+    (code >= 0x09 && code <= 0x0D)
+        || (code == 0x20)
+        -- Latin-1 whitespace
+        || (code == 0x85)
+        || (code == 0xA0)
+        -- Unicode whitespace (category Z)
+        || (code == 0x1680)
+        || (code >= 0x2000 && code <= 0x200A)
+        || (code == 0x2028)
+        || (code == 0x2029)
+        || (code == 0x202F)
+        || (code == 0x205F)
+        || (code == 0x3000)
+
+
+syntaxChars : Set.Set Char
+syntaxChars =
+    Set.fromList [ '{', '}', '!', '=', '~', ',', '\\', '"', '\'', '`' ]
 
 
 withMatchers : List Matcher -> Filter -> Filter
