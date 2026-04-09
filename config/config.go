c3e408aa6866c11cd3b8287074c701987a46420b
Asish Kumar <officialasishkumar@gmail.com>
2026-04-09 18:17:03 +0000

    config: honor global slack_api_url when validating update_message
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- config/config.go
+++ config/config.go
@@ -464,6 +464,9 @@ func (c *Config) UnmarshalYAML(unmarshal func(any) error) error {
 				}
 				sc.APIURL = (*amcommoncfg.SecretURL)(sc.AppURL)
 			}
+			if err := slack.ValidateUpdateMessageAPIURL(sc.UpdateMessage, sc.APIURL); err != nil {
+				return err
+			}
 		}
 		for _, poc := range rcv.PushoverConfigs {
 			if poc == nil {
